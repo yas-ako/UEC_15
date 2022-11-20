db3917a7ee0de2cb778b3830976578dbed02f59f
ypd14 <118607194+ypd14@users.noreply.github.com>
2022-11-20 22:58:24 +0900

    hai

--- src/layer/layer_1.c
+++ src/layer/layer_1.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "..\img.h"
+#include "img.h"
 
-//背景　富士山
-void layer_1(double t)
-{
-  struct color c3 = {255, 0, 0};
-  img_fillcircle(c3, t, t, 10);
+
+int main(void) {
+  struct color c1={0,0,255};
+  struct color c2={255,255,255};
+
+  int i;
+  for(i=0;i<20;++i) {
+    img.clear();
+   img_filltriangle(c1,120+i*8,180,20+i*8,20,140+i*8,180,120+i*8,180,240+i*8,20,240+i*8,20);
+   img_filltriangle(c2,120+i*8,180,100+i*8,160,140+i*8,180,120+i*8,180,160+i*8,160,160+i*8,160);
+   img_write();
+  }
 }
