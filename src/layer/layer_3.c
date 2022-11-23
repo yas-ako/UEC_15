659574f24178b14819fac8c089ba9ff10d3ceae4
yas-ako <105139975+yas-ako@users.noreply.github.com>
2022-11-23 09:31:11 +0900

    building

--- src/layer/layer_3.c
+++ src/layer/layer_3.c
@@ -2,9 +2,109 @@
 #include <stdlib.h>
 #include "..\img.h"
 
+int x1 = 0;
+int y1_ = 0;
+int x2 = 0;
+int y2 = 0;
+int x3 = 0;
+int y3 = 0;
+
 //市街地
+
+void img_fillrect(struct color c, double x, double y, double w, double h)
+{
+  int wmin = (int)(x), wmax = (int)(x + w);
+  int hmin = (int)(y), hmax = (int)(y + h);
+  int i, j;
+  for (j = hmin; j <= hmax; ++j)
+  {
+    for (i = wmin; i <= wmax; ++i)
+    {
+      img_putpixel(c, i, j);
+    }
+  }
+}
+
+//市街地
+//建物サイズを決める
+//建物の左下の座標を計算
+int lay_3_xy(double t)
+{
+  // int n = t / 720;
+  // int y1_offset = 0;
+  // int x1 = (int)(t + 0.5) % 60 + 720 * n;
+  // int  y1_ = y1_offset;
+
+  // int y2_offset = 0;
+  // int x2 = (int)(t + 0.5) % 60 - 200 + 720 * n;
+  // int y2 = y2_offset;
+
+  // int y3_offset = 0;
+  // int x3 = (int)(t + 0.5) % 60 - 500 + 720 * n;
+  // int y3 = y3_offset;
+
+  int n = t / WIDTH;
+  int y1_offset = 0;
+  // x1 = (int)(t + 0.5) % 60 + 720 * n * 600;
+  x1 = (int)(t * 600 + 0.5) % WIDTH;
+  y1_ = y1_offset;
+
+  int y2_offset = 0;
+  x2 = (int)(t + 0.5) % 60 - 200 + 720 * n * 600;
+  y2 = y2_offset;
+
+  int y3_offset = 0;
+  x3 = (int)(t + 0.5) % 60 - 500 + 720 * n * 600;
+  y3 = y3_offset;
+};
+
+//建物の色指定
+struct color c3_1 = {255, 255, 255};
+struct color c3_2 = {255, 150, 150};
+struct color c3_3 = {150, 150, 150};
+
+void layer_3_1(double t)
+{
+  lay_3_xy(t);
+  struct color c = c3_1;
+  double x = x1;
+  double y = y1_;
+  double w = 30;
+  double h = 28;
+
+  img_fillrect(c, x, y, w, h);
+}
+
+void layer_3_2(double t)
+{
+  lay_3_xy(t);
+  struct color c = c3_2;
+  double x = x2;
+  double y = y2;
+  double w = 288;
+  double h = 180;
+
+  img_fillrect(c, x, y, w, h);
+}
+
+void layer_3_3(double t)
+{
+  lay_3_xy(t);
+  struct color c = c3_3;
+  double x = x3;
+  double y = y3;
+  double w = 150;
+  double h = 250;
+
+  img_fillrect(c, x, y, w, h);
+}
+
 void layer_3(double t)
 {
   struct color c3 = {40, 55, 0};
-  img_fillcircle(c3, t * 3, t * 3, 10);
+  // img_fillcircle(c3, t * 3, t * 3, 10);
+  // lay_3_xy(t);
+  layer_3_1(t);
+  // layer_3_2(t);
+  // layer_3_3(t);
 }
--- src/layer/layer_5.c
+++ src/layer/layer_5.c
@@ -5,7 +5,6 @@
 //窓と電柱
 void layer_5(double t)
 {
-struct color c5={128,128,128};
-img_fillline(c5,0+i*8,480,0+i*8,0,25);
+  struct color c5 = {128, 128, 128};
+  img_fillline(c5, 0 + t * 8, 480, 0 + t * 8, 0, 25);
 }
-
